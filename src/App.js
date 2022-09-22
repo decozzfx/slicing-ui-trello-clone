4c84adf2776b02fa7769ad3480debc7dc26e4a49
moch.fathurrozi <moch.fathurrozi@ist.id>
2022-09-22 21:32:22 +0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Cards from './Components/Cards';
+import Container from './Components/Container';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1 className='text-xl mt-5'>Product Roadmap</h1>
+      <Cards/>
+
+    </Container>
   );
 }
 
---
+++ src/Components/Cards.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './Card'
+
+const Cards = () => {
+  return (
+    <div className='mt-5'>
+        <Card/>
+    </div>
+  )
+}
+
+export default Cards
